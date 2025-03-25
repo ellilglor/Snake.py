e1561b5856c5f81fbedb862ac0764e501da84ec7
ellilglor <thomassmekens14@gmail.com>
2025-03-25 15:55:31 +0100

    Finished prototype

--- Src/Classes/apple.py
+++ Src/Classes/apple.py
@@ -1,3 +1,4 @@
 class Apple(object):
-    def __init__(self, location):
+    def __init__(self, location, points):
         self.location = location
+        self.points = points
--- Src/Classes/game.py
+++ Src/Classes/game.py
@@ -1,4 +1,6 @@
+import random
 import Classes.snake as snake
+import Classes.apple as apple
 
 class Game(object):
     EMPTY = 0
@@ -22,17 +24,20 @@ class Game(object):
     INPUT_DOWN = "S"
     INPUT_LEFT = "A"
     INPUT_RIGHT = "D"
+    INPUT_END = "E"
 
     def __init__(self, height, width):
         self.height = height
         self.width = width
-
+        self.score = 0
         self.snake = snake.Snake([(0, 0), (1, 0), (2, 0), (3, 0), (4, 0)], self.UP)
     
     def play(self):
-        self.__renderBoard()
+        self.__generateApple()
 
         while (True):
+            self.__renderBoard()
+
             char = input().upper()
 
             if char == self.INPUT_UP and self.snake.direction != self.DOWN:
@@ -43,15 +48,24 @@ class Game(object):
                 self.snake.setDirection(self.LEFT)
             elif char == self.INPUT_RIGHT and self.snake.direction != self.LEFT:
                 self.snake.setDirection(self.RIGHT)
+            elif char == self.INPUT_END:
+                break
 
             next_position = self.__nextPosition(self.snake.head(), self.snake.direction)
             if next_position in self.snake.body:
                 break
 
-            self.snake.takeStep(next_position)
+            if next_position == self.apple.location:
+                self.score += self.apple.points
+                self.snake.extendBody(next_position)
+                self.__generateApple()
+            else:
+                self.snake.takeStep(next_position)
 
-            print(self.snake.body)
-            self.__renderBoard()
+        self.__finishGame()
+        
+    def __finishGame(self):
+        print(f"You finished with a score of {self.score}.")
 
     def __buildBoard(self):
         board = [[self.EMPTY for _ in range(self.height)] for _ in range(self.width)]
@@ -62,7 +76,18 @@ class Game(object):
         head = self.snake.head()
         board[head[0]][head[1]] = self.HEAD
 
+        board[self.apple.location[0]][self.apple.location[1]] = self.APPLE
+
         return board
+    
+    def __generateApple(self):
+        while (True):
+            location = (random.randint(0, self.width - 1), random.randint(0, self.height - 1))
+            if (location not in self.snake.body):
+                break
+        
+        points = 1
+        self.apple = apple.Apple(location, points)
     
     def __nextPosition(self, position, step):
         return ((position[0] + step[0]) % self.width, (position[1] + step[1]) % self.height)
